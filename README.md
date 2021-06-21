b976dd36e22a43c3da93fdc9620dd4107dea6936
JoeBig7 <1479762535@qq.com>
2021-06-21 15:19:24 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## A crawler example
---
+++ src/main/java/bootstrap/Hello.java
@@ -0,0 +1,12 @@
+package bootstrap;
+
+/**
+ * @Author JoeBig7
+ * @date 2021/6/21 15:10:38
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
